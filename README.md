bb1f729de771a16e41ce3a46602315f6a5fd90fa
Jo Hanna Pearce <jo@nrwl.io>
2020-01-02 17:13:08 +0000

    feat : add move schematic illustration

--- README.md
+++ README.md
@@ -1,28 +1,16 @@
-# Getting Started With Schematics
+# A basic reproduction of the "This should never happen" error.
 
-This repository is a basic Schematic implementation that serves as a starting point to create and publish Schematics to NPM.
-
-### Testing
-
-To test locally, install `@angular-devkit/schematics-cli` globally and use the `schematics` command line tool. That tool acts the same as the `generate` command of the Angular CLI, but also has a debug mode.
-
-Check the documentation with
-```bash
-schematics --help
-```
+This repository is a basic Schematic implementation that illustrates a bug.
 
 ### Unit Testing
 
 `npm run test` will run the unit tests, using Jasmine as a runner and test framework.
 
-### Publishing
-
-To publish, simply do:
+Output should look as follows :
 
 ```bash
-npm run build
-npm publish
+Failures:
+1) move-schematic works
+  Message:
+    Error: This should never happen. Path: "/projects/bar".
 ```
-
-That's it!
- 
--- src/move-schematic/index_spec.ts
+++ src/move-schematic/index_spec.ts
@@ -1,3 +1,4 @@
+import { Tree } from '@angular-devkit/schematics';
 import {
   SchematicTestRunner,
   UnitTestTree
@@ -8,6 +9,8 @@ import {
 } from '@schematics/angular/application/schema';
 import { Schema as WorkspaceOptions } from '@schematics/angular/workspace/schema';
 import * as path from 'path';
+import { moveSchematic } from '.';
+import { Schema } from './schema';
 
 const collectionPath = path.join(__dirname, '../collection.json');
 
@@ -33,30 +36,68 @@ describe('move-schematic', () => {
 
   beforeEach(async () => {
     runner = new SchematicTestRunner('schematics', collectionPath);
-    tree = runner.runExternalSchematic(
-      '@schematics/angular',
-      'workspace',
-      workspaceOptions
-    );
-    tree = runner.runExternalSchematic(
-      '@schematics/angular',
-      'application',
-      appOptions,
-      tree
-    );
+    tree = new UnitTestTree(Tree.empty());
+
+    tree = await runner
+      .runExternalSchematicAsync(
+        '@schematics/angular',
+        'workspace',
+        workspaceOptions,
+        tree
+      )
+      .toPromise();
+
+    tree = await runner
+      .runExternalSchematicAsync(
+        '@schematics/angular',
+        'application',
+        appOptions,
+        tree
+      )
+      .toPromise();
   });
 
-  it('works', () => {
+  it('works', async () => {
+    const schema: Schema = {
+      source: 'projects/bar',
+      destination: 'projects/foo'
+    };
+
+    tree = (await runner
+      .callRule(moveSchematic(schema), tree)
+      .toPromise()) as UnitTestTree;
+
     expect(tree.files).toEqual([
       '/README.md',
+      '/.editorconfig',
+      '/.gitignore',
       '/angular.json',
       '/package.json',
       '/tsconfig.json',
       '/tslint.json',
-      '/.editorconfig',
-      '/.gitignore',
-      '/projects/bar/src/app/test/test.spec.ts',
-      '/projects/bar/src/app/test/test.ts'
+      '/projects/foo/browserslist',
+      '/projects/foo/karma.conf.js',
+      '/projects/foo/tsconfig.app.json',
+      '/projects/foo/tsconfig.spec.json',
+      '/projects/foo/tslint.json',
+      '/projects/foo/src/favicon.ico',
+      '/projects/foo/src/index.html',
+      '/projects/foo/src/main.ts',
+      '/projects/foo/src/polyfills.ts',
+      '/projects/foo/src/styles.css',
+      '/projects/foo/src/test.ts',
+      '/projects/foo/src/assets/.gitkeep',
+      '/projects/foo/src/environments/environment.prod.ts',
+      '/projects/foo/src/environments/environment.ts',
+      '/projects/foo/src/app/app.module.ts',
+      '/projects/foo/src/app/app.component.css',
+      '/projects/foo/src/app/app.component.html',
+      '/projects/foo/src/app/app.component.spec.ts',
+      '/projects/foo/src/app/app.component.ts',
+      '/projects/foo/e2e/protractor.conf.js',
+      '/projects/foo/e2e/tsconfig.json',
+      '/projects/foo/e2e/src/app.e2e-spec.ts',
+      '/projects/foo/e2e/src/app.po.ts'
     ]);
   });
 });
